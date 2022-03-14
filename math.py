9b3f842d81159f600c1a33fd1cc01df0afde2ea2
tanmay9724 <72390807+tanmay9724@users.noreply.github.com>
2022-03-15 01:06:16 +0530

    updates comments on sub() fn

--- math.py
+++ math.py
@@ -2,11 +2,11 @@
 def add(x,y):
 	return (x+y)
 # subtraction fn()
-def subtract(x,y):
+def subtract(x,y):        # sub() fn
 	return (x-y)
 # multiplication fn()
 def multiply(x,y):
 	return (x*y)
 #division fn()
-def divide(x,y):
+def divide(x,y): 
 	pass
